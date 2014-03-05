14cfc08874fb92674a2287ef17e6d61f0804dc5e
IvanZhao <zyf84109@gmail.com>
2014-03-05 22:01:54 +0800

    SnowBall: Adding pause action when jump

--- src/com/ivan/snowball/model/Ball.java
+++ src/com/ivan/snowball/model/Ball.java
@@ -10,13 +10,71 @@ import android.graphics.Paint;
 import android.graphics.Point;
 
 public class Ball extends Element {
+    private final float SPEED0 = 10;
     private int mLife = 100;
     private Point mPosition = null;
     private Point mBlockPosition = null;
     private Ground mGround = null;
     private float mBallRotate = 0;
-    private float v0 = 10;
+    private double v0 = 0;
+    private double vt = 0;
     private boolean mJumping = false;
+    private Thread mJumpThread = new Thread() {
+
+        @Override
+        public void run() {
+            try {
+                mJumping = true;
+                long t0 = System.currentTimeMillis();
+                double t;
+                Double s = 0.0;
+                boolean start = false;
+                boolean jumping = true;
+                boolean rebounding = true;
+                while(jumping && mJumping) {
+                    t = (System.currentTimeMillis() - t0) / 1000.0;
+                    s = (v0 * t + SnowBallUtil.G * t * t) * 100;
+                    vt = v0 + SnowBallUtil.G * t;
+                    if(!start && mPosition.y < mBlockPosition.y) {
+                        start = true;
+                    }
+                    if(mPosition.y >= mBlockPosition.y) {
+                        mPosition.y = mBlockPosition.y;
+                    } else {
+                        mPosition.y = mBlockPosition.y - s.intValue();
+                    }
+                    if(start && mPosition.y == mBlockPosition.y) {
+                        jumping = false;
+                    }
+                }
+                t0 = System.currentTimeMillis();
+                start = false;
+                if(Math.abs(vt) < 3) {
+                    mJumping = false;
+                    return;
+                }
+                while(rebounding && mJumping) {
+                    t = (System.currentTimeMillis() - t0) / 1000.0;
+                    s = (v0 / 3 * t + SnowBallUtil.G * t * t) * 100;
+                    if(!start && s.intValue() > 0) {
+                        start = true;
+                    }
+                    if(s.intValue() <= 0) {
+                        mPosition.y = mBlockPosition.y;
+                    } else {
+                        mPosition.y = mBlockPosition.y - s.intValue();
+                    }
+                    if(start && s.intValue() <= 0) {
+                        rebounding = false;
+                    }
+                }
+                mJumping = false;
+                gotHurt(Hurts.FALL);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
     public Ball(Bitmap ballImage, Ground ground, int canvasH, int canvasW) {
         super(ballImage, canvasH, canvasW);
@@ -47,60 +105,18 @@ public class Ball extends Element {
     }
 
     public void jump() {
-        if(mJumping) {
+        if(mJumping && vt > 0) {
             return;
         }
-        mJumping = true;
-        new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    long t0 = System.currentTimeMillis();
-                    double t;
-                    Double s = 0.0;
-                    boolean start = false;
-                    boolean jumping = true;
-                    boolean rebounding = true;
-                    while(jumping) {
-                        t = (System.currentTimeMillis() - t0) / 1000.0;
-                        s = (v0 * t + SnowBallUtil.G * t * t) * 100;
-                        if(!start && s.intValue() > 0) {
-                            start = true;
-                        }
-                        if(s.intValue() <= 0) {
-                            mPosition.y = mBlockPosition.y;
-                        } else {
-                            mPosition.y = mBlockPosition.y - s.intValue();
-                        }
-                        if(start && s.intValue() <= 0) {
-                            jumping = false;
-                        }
-                    }
-                    t0 = System.currentTimeMillis();
-                    start = false;
-                    while(rebounding) {
-                        t = (System.currentTimeMillis() - t0) / 1000.0;
-                        s = (v0 / 3 * t + SnowBallUtil.G * t * t) * 100;
-                        if(!start && s.intValue() > 0) {
-                            start = true;
-                        }
-                        if(s.intValue() <= 0) {
-                            mPosition.y = mBlockPosition.y;
-                        } else {
-                            mPosition.y = mBlockPosition.y - s.intValue();
-                        }
-                        if(start && s.intValue() <= 0) {
-                            rebounding = false;
-                        }
-                    }
-                    mJumping = false;
-                    gotHurt(Hurts.FALL);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+        if(!mJumping) {
+            v0 = SPEED0;
+        } else {
+            v0 = -vt / 2;
+        }
+        if(mJumpThread.isAlive()) {
+            mJumping = false;
+        }
+        mJumpThread.start();
     }
 
     @Override
